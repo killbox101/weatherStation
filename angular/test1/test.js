ada2f36405316f9c8769df3ac1e85a6ec32f7563
killbox101 <lucas_jacob_999@hotmail.com>
2015-06-17 23:58:08 +1000

    tesr

--- angular/test1/test.js
+++ angular/test1/test.js
@@ -1,42 +1,20 @@
-//var app = angular.module("weatherStation", [])
-//app.controller("wsController", function ($scope,$http){
-//
-//	$http({method:'GET',url:'http:192.168.2.10:5000/weatherToday/'})
-////$http({method:'GET', url:'http://movieapp-sitepointdemos.rhcloud.com/api/movies'}).success(function(data)
-//    success(function(data) {
-//    $scope.weather = data;
-//	});
-//
-
 
 // Code goes here
 
 var myApp = angular.module("myApp", [])
-		.controller('LabController', function($scope, $http) {
+		.controller('weatherController', function($scope, $http) 
+		{
         
-		$scope.status="Hello ";
-  // http://www.listingsearch.com/service/test_function  please check its returns one onject 
-  
-		//$http({method:'GET', url:'http://movieapp-sitepointdemos.rhcloud.com/api/movies'})
-			$http({method:'GET',url:'http://192.168.2.10:5000/weatherToday/'}).success(function(data)
-{
- 
-//alert($scope.posts);
- 
-$scope.movies = data; // response data
-//alert($scope.movies); 
-});
+			$scope.status="Hello ";
+	  		
+			$http({method:'GET',url:'http://192.168.2.10:5000/weatherToday/'})
+			.success(function(data)
 
+			{
+			 
+			$scope.wRecord = data.items; // response data
+			//alert($scope.wRecord)
+			
+			});
 		
-		
-		
-		/*$scope.batches = [
-			{id: "2014BATCH50", status: "QC in Progress. Illumina: 24/50",  date: "10/24/14", qlty: "check", illumina: "none, raw, modified, modified", FX: "modified"},
-			{id: "2014BATCH49", status: "Pre-Extraction",  date: "10/24/14", date: "10/24/14", qlty: "check", illumina: "none, raw, modified, modified", FX: "modified"},
-			{id: "2014BATCH48", status: "Awaiting Raw Uploads", date: "10/24/14", date: "10/24/14", qlty: "check", illumina: "none, raw, modified, modified", FX: "modified"},
-			{id: "2014BATCH47", status: "Pre-Extraction", date: "10/24/14", date: "10/24/14", qlty: "check", illumina: "none, raw, modified, modified", FX: "modified"},
-			{id: "2014BATCH46", status: "Awaiting Raw & Modified Uploads", date: "10/24/14", date: "10/24/14", qlty: "check", illumina: "none, raw, modified, modified", FX: "modified"},
-			{id: "2014BATCH46", status: "Awaiting Raw & Modified Uploads", date: "10/24/14", date: "10/24/14", qlty: "check", illumina: "none, raw, modified, modified", FX: "modified"}
-		];*/
-		
-	}); 
+		}); 
